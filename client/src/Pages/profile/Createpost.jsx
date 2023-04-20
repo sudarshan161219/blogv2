1d40c7605f0e3a855d4956013fab04f661bae20c
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-04-20 11:42:44 +0530

    11:42am

--- client/src/Pages/profile/Createpost.jsx
+++ client/src/Pages/profile/Createpost.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 import Wrapper from "../../assets/Wrappers/Createpost";
+
+
 const Createpost = () => {
   const [value, setValue] = useState("");
 
-
-  const addDetails = () => {
-
-  }
- 
+  const addDetails = () => {};
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+    ],
+    content: '<p>Hello World!</p>',
+  })
   return (
     <>
       <Wrapper className="container">
         <div className="row">
           <form onSubmit={addDetails} className="quill-form">
-            <h3 > Add </h3>
+            <h3> Add </h3>
             <div className="form-row">
               <div className="title-input">
                 <label className="font-weight-bold">
@@ -31,37 +37,8 @@ const Createpost = () => {
                   required
                 />
               </div>
-              <div className="clearfix"></div>
-              <div className="form-group col-md-12 editor">
-                <label className="font-weight-bold">
-                  Description <span className="required"> * </span>
-                </label>
-                {/* <EditorToolbar toolbarId={"t1"} /> */}
-                <ReactQuill
-                  theme="snow"
-                  // value={userInfo.description}
-                  // onChange={ondescription}
-                  placeholder={"Write something awesome..."}
-                  // modules={modules("t1")}
-                  // formats={formats}
-                />
-              </div>
               <br />
-              <div className="form-group col-md-12 editor">
-                <label className="font-weight-bold">
-                  Additional Information{" "}
-                </label>
-                {/* <EditorToolbar toolbarId={"t2"} /> */}
-                <ReactQuill
-                  theme="snow"
-                  // value={userInfo.information}
-                  // onChange={oninformation}
-                  placeholder={"Write something awesome..."}
-                  // modules={modules('t2')}
-                  // formats={formats}
-                />
-              </div>
-              <br />
+              <EditorContent editor={editor} />
               {/* {isError !== null && <div className="errors"> {isError} </div>} */}
               <div className="btn-container">
                 <button type="submit" className="button-28 quill-btn">
--- client/src/assets/Wrappers/Createpost.js
+++ client/src/assets/Wrappers/Createpost.js
@@ -5,21 +5,35 @@ const Wrapper = styled.div`
   color: #2b2b2b;
   letter-spacing: 1px;
   padding: 1rem;
-  /* .row { */
   background-color: transparent;
   background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23000000' fill-opacity='0.13' fill-rule='evenodd'/%3E%3C/svg%3E");
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
-  /* } */
+h3{
+    margin-bottom: 1rem;
+}
+
+  .quill{
+    /* flex-wrap: wrap; */
+    width: 100%;
+  }
+
   .quill-form {
-    display: grid;
+    /* display: grid; */
     gap: 1rem;
   }
 
   .title-input {
     display: grid;
     gap: 0.7rem;
-    margin-bottom: 1.5rem;
+  }
+
+  .form-group {
+    margin-top: 2rem;
+  }
+
+  .editor {
+    margin-bottom: 1rem;
   }
 
   .title-input > input {
