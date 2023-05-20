29f5684d70bb1122b47d1d647056a0b2f36fa3db
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-05-20 13:17:49 +0530

    01:17pm

--- api/controllers/postController.mjs
+++ api/controllers/postController.mjs
@@ -70,11 +70,25 @@ const authorPosts = async (req, res) => {
     result = result.sort("createdAt");
   }
 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) ||10
+
+
+
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
+
   const authorpost = await result
+
+  const totalPosts = await Post.countDocuments(queryObject)
+  const numOfPages = Math.ceil (totalPosts / limit)
 
   return res.status(StatusCodes.OK).json({
     authorpost,
-    totalPost: authorpost.length,
+    totalPosts,
+    numOfPages
   });
 };
 
