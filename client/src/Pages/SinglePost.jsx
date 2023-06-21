34bcf798aebb0831f44a2683e745d648276788a1
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-06-21 18:42:16 +0530

    06:42pm

--- client/src/Pages/SinglePost.jsx
+++ client/src/Pages/SinglePost.jsx
@@ -2,15 +2,47 @@ import React, { useEffect } from "react";
 import { useAppContext } from "../context/Context";
 import { Link, useParams } from "react-router-dom";
 import Wrapper from "../assets/Wrappers/SinglePost";
+import moment from "moment";
+import { Loading } from "../Components/export";
+import { BsDot } from "react-icons/bs";
 const SinglePost = () => {
-  const { getSinglePost } = useAppContext();
+  const { getSinglePost, post, isLoading } = useAppContext();
   const { id } = useParams();
+
+  const { _id, title, coverImg, content, createdAt, author } = post;
+
+  const date = moment(createdAt);
+  let Fdate = date.format("MMM Do, YYYY");
+
   useEffect(() => {
     getSinglePost(id);
   }, [id]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Wrapper>
-      <div>SinglePost</div>
+      <div className="post-heading-container">
+        <h1>{title}</h1>
+        <div className="post-date-author-info-container">
+          <span>{Fdate}</span>
+          <BsDot />
+          <span>{author ? author.name : null}</span>
+        </div>
+      </div>
+
+      <div className="post-img-container">
+        <img src={coverImg} alt={title} />
+      </div>
+
+      <div className="ql-snow post-content-container">
+        <div
+          className="ql-editor"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+      </div>
     </Wrapper>
   );
 };
--- client/src/assets/Wrappers/SinglePost.js
+++ client/src/assets/Wrappers/SinglePost.js
@@ -1,6 +1,52 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  .post-heading-container {
+    display: grid;
+    gap: 1rem;
+    text-align: center;
+    margin-bottom: 3rem;
+    padding: 1rem;
+  }
+
+  .post-heading-container > h1 {
+    font-family: FontBold;
+    font-size: 26px;
+    line-height: 120%;
+  }
+
+  .post-date-author-info-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .post-date-author-info-container > span {
+    text-transform: uppercase;
+    font-family: FontSemiBold;
+    font-size: 13px;
+    line-height: 120%;
+  }
+
+  .post-date-author-info-container > span:nth-child(1) {
+    color: #475467;
+  }
+
+  .post-date-author-info-container > span:last-child {
+    color: #1d2939;
+  }
+
+  .post-img-container {
+    position: relative;
+  }
+
+  .post-img-container > img {
+    width: 100%;
+  }
+
+  .post-content-container {
+    padding: 1rem;
+  }
   @media (min-width: 700px) {
     padding: 0 2rem;
   }
