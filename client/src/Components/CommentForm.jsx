30581f7d57019b06ba1b0645de2853d4d38b6e38
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-06-27 18:45:12 +0530

    06:45pm

--- client/src/Components/CommentForm.jsx
+++ client/src/Components/CommentForm.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import Wrapper from "../assets/Wrappers/CommentForm";
+import { useAppContext } from "../context/Context";
+
 const CommentForm = () => {
+  const { user } = useAppContext();
+  console.log(user);
   return (
     <Wrapper>
       <form>
-        <textarea
-          placeholder="post your comment"
-          name=""
-          id=""
-          className="comment-form"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <button className="button-28 comment-btn">submit</button>
+        <div className="text-img-container">
+          <img
+            className="desktop-comment-img"
+            src={user.userImg}
+            alt={user.name}
+          />
+
+          <textarea
+            placeholder="post your comment"
+            name=""
+            id=""
+            className="comment-form"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <div className="img-btn">
+          <img
+            className="mobile-comment-img"
+            src={user.userImg}
+            alt={user.name}
+          />
+          <button className="button-28 comment-btn">submit</button>
+        </div>
       </form>
     </Wrapper>
   );
--- client/src/assets/Wrappers/CommentForm.js
+++ client/src/assets/Wrappers/CommentForm.js
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
     display: grid;
     background-color: #fff;
     box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    padding: 0.3rem;
+    padding: 1rem;
     border-radius: 10px;
   }
   .comment-form {
@@ -21,6 +21,24 @@ const Wrapper = styled.div`
     font-family: FontRegular;
     letter-spacing: 1.1px;
     line-height: 150%;
+    border-radius: 8px;
+    margin-bottom: 1rem;
+    padding: 0.2rem;
+  }
+  .comment-form:focus{
+    outline: gray solid 1px;
+  }
+
+  .img-btn {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .mobile-comment-img {
+    width: 35px;
+    border-radius: 100%;
+    overflow: hidden;
   }
 
   .comment-btn {
@@ -30,6 +48,38 @@ const Wrapper = styled.div`
     width: 100px;
     display: flex;
     justify-self: end;
+  }
+
+  .desktop-comment-img {
+    display: none;
+  }
+
+  @media (min-width: 700px) {
+    .mobile-comment-img {
+      display: none;
+    }
+
+    .img-btn {
+    justify-content:end;
+  }
+
+    .desktop-comment-img {
+      display: block;
+      width: 40px;
+      border-radius: 100%;
+      overflow: hidden;
+    }
+
+    .text-img-container {
+      display: flex;
+      align-items: start;
+      gap: 1rem;
+    }
+
+form {
+    width: 650px;
+    margin: 0 auto;
+}
   }
 `;
 
