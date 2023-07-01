391c257f250b044ee99f21e37bc9332ff887f083
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-07-01 09:57:27 +0530

    09:57am

--- client/src/Components/CommentForm.jsx
+++ client/src/Components/CommentForm.jsx
@@ -3,18 +3,16 @@ import Wrapper from "../assets/Wrappers/CommentForm";
 import { Navigate } from "react-router-dom";
 import { useAppContext } from "../context/Context";
 import { Link, useParams } from "react-router-dom";
-import Loading from "../assets/Rolling-0.7s-157px.svg"
+import Loading from "../assets/Rolling-0.7s-157px.svg";
 const CommentForm = () => {
   const [text, setText] = useState("");
   const { user, createComment, formLoading, getComments } = useAppContext();
 
   const { id } = useParams();
 
-  if (!user) {
-    return <Navigate to="/" />;
-  }
-
-
+  // if (!user) {
+  //   return <Navigate to="/" />;
+  // }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,7 +25,7 @@ const CommentForm = () => {
     } else {
       createComment(data);
       e.currentTarget.reset();
-      setText("")
+      setText("");
     }
   };
 
@@ -37,37 +35,56 @@ const CommentForm = () => {
 
   return (
     <Wrapper>
-      <form onSubmit={handleSubmit}>
-        <div className="text-img-container">
-          <img
-            className="desktop-comment-img"
-            src={user && user.userImg}
-            alt={user && user.name}
-          />
+      {user ? (
+        <form onSubmit={handleSubmit}>
+          <div className="text-img-container">
+            <img
+              className="desktop-comment-img"
+              src={user && user.userImg}
+              alt={user && user.name}
+            />
 
-          <textarea
-            value={text}
-            onChange={handleChange}
-            placeholder="post your comment"
-            name="content"
-            id="content"
-            className="comment-form"
-            cols="30"
-            rows="10"
-          ></textarea>
+            <textarea
+              value={text}
+              onChange={handleChange}
+              placeholder="post your comment"
+              name="content"
+              id="content"
+              className="comment-form"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <div className="img-btn">
+            <img
+              className="mobile-comment-img"
+              src={user.userImg}
+              alt={user.name}
+              disabled={formLoading}
+            />
+            <button className="button-28 comment-btn">
+              {formLoading ? (
+                <img className="giff" src={Loading} alt="" />
+              ) : (
+                "submit"
+              )}
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="ldc-disable-msg">
+          <strong>
+            <Link className="strong-link" to="/register">
+              login
+            </Link>
+            &nbsp; or &nbsp;
+            <Link className="strong-link" to="/register">
+              sign up
+            </Link>
+            &nbsp; to like and comment on post
+          </strong>
         </div>
-        <div className="img-btn">
-          <img
-            className="mobile-comment-img"
-            src={user.userImg}
-            alt={user.name}
-            disabled={formLoading}
-          />
-          <button className="button-28 comment-btn">
-            {formLoading ? <img  className="giff" src={Loading} alt="" /> : "submit"}
-          </button>
-        </div>
-      </form>
+      )}
     </Wrapper>
   );
 };
