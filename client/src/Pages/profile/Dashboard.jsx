1d0b8f7e45cab37029d38947d77aa6c21fbc8880
unknown <godhustler90956@gmail.com>
2023-08-05 13:10:18 +0530

    01:10pm

--- client/src/Pages/profile/Dashboard.jsx
+++ client/src/Pages/profile/Dashboard.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Wrapper from '../../assets/Wrappers/Dashboard'
+import { AiOutlineEye } from "react-icons/ai"
+import { GoPencil } from "react-icons/go"
+
 const Dashboard = () => {
   return (
     <Wrapper>
-    <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+      <div className="stats-container">
+        <div className="total-views-container">
+          <strong>total views</strong>
+          <div className='dash-stats'>
+            <AiOutlineEye />
+            <span>215k</span>
+          </div>
+        </div>
+        <div className="total-posts-container">
+          <strong>total posts</strong>
+          <div className='dash-stats'>
+            <GoPencil />
+            <span>215k</span>
+          </div>
+        </div>
+        <div className="most-viewed-article-container">
+          <strong>Most viewed article </strong>
+          <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ab nulla culpa accusantium, consectetur nostrum.</span>
+        </div>
+      </div>
+{/* chart */}
     </Wrapper>
   )
 }
--- client/src/assets/Wrappers/Dashboard.js
+++ client/src/assets/Wrappers/Dashboard.js
@@ -3,6 +3,34 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   padding: 1rem;
 
+  .stats-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+    margin-top: 2rem;
+  }
+
+  .stats-container > div {
+    border: 1px solid #000;
+    padding: 1rem;
+    display: grid;
+    gap: 0.5rem;
+  }
+
+  .dash-stats {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .most-viewed-article-container {
+  }
+
+  .most-viewed-article-container > strong,
+  span {
+    text-align: left;
+  }
 `;
 
 export default Wrapper;
