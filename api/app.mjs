b6d56282fc27f0ca57d723e6bbcd341682080a48
unknown <godhustler90956@gmail.com>
2023-08-08 03:15:50 +0530

    deploy v5

--- api/app.mjs
+++ api/app.mjs
@@ -29,19 +29,25 @@ const corsOrigin = {
   optionSuccessStatus: 200,
 };
 
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
-// app.use(helmet({
-//   contentSecurityPolicy: {
-//     directives: {
-//       defaultSrc: ["'self'"],
-//       connectSrc: ["'self'", 'http://127.0.0.1:8000', 'ws://localhost:42877/']
-//     }
-//   }
-// }));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: [
+          "'self'",
+          "http://127.0.0.1:8000",
+          "ws://localhost:42877/",
+        ],
+        "img-src": ["'self'", "https: data:"],
+      },
+    },
+  })
+);
 app.use(xss());
 app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
